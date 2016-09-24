6284723d32f9dec23d67d8915df620dfd21fade4
Dan Fox <dan@derbyjackpot.com>
2016-09-23 20:39:22 -0400

    clean up definition a bit

--- lib/zygote/definition.rb
+++ lib/zygote/definition.rb
@@ -2,10 +2,10 @@ class Zygote::Definition
   def initialize(name: nil, model_class:, keys: [:id], attributes:)
     @name = name
     @model_class = model_class
-    @attributes = attributes.symbolize_keys
+    @attributes = attributes
     @keys = keys
 
-    raise ArgumentError unless @keys.all? { |key| @attributes.has_key?(key) }
+    raise ArgumentError unless all_keys_specified?
   end
 
   attr_reader :name, :model_class, :attributes, :keys
@@ -19,5 +19,9 @@ class Zygote::Definition
 
   def key_attributes
     attributes.slice(*keys)
+  end
+
+  def all_keys_specified?
+    keys.all? { |key| attributes.has_key?(key) }
   end
 end
